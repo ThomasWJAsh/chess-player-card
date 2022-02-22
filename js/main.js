17781207ad4c3465add4d27e7adce8fe8adc778b
ThomasWJAsh <thomas.ash@bath,edu>
2022-02-22 16:26:13 +0000

    pseudo coding

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+//Create an object constructor with properties of website,
+//rapid, blitz and bullet
+
+//fetch rapid, blitz, bullet data and store in
+// a dotCom object and a lichess object
+
+//for each format, get the data from the objects
+
+
 fetch('https://api.chess.com/pub/player/tashable')
 .then(response => response.json())
 .then(data => console.log(data));
